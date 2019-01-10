44ea10964c5ef94c19c81e1550de1e230a0e6916
ljm <123@qq.com>
2019-01-11 00:31:37 +0800

    Initial commit

---
+++ src/main/com/qf/test/Demo.java
@@ -0,0 +1,11 @@
+package com.qf.test;
+
+/**
+ * @author LJM
+ * @Date 2019/1/11
+ */
+public class Demo {
+    public static void main(String[] args){
+        System.out.println("Hello GitHub");
+    }
+}
